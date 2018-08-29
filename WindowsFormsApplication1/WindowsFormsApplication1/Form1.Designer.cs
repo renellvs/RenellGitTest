efd77efbccdd4691edc8c74b0ecd25a0ab60c899
efram76 <eric.framke@mymail.nwtc.edu>
2018-08-28 19:10:54 -0500

    New font color and added button

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.lblHello = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblHello
@@ -41,11 +42,21 @@
             this.lblHello.TabIndex = 0;
             this.lblHello.Text = "Testing Testing testing Testing";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblHello);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label lblHello;
+        private System.Windows.Forms.Button button1;
     }
 }
 
